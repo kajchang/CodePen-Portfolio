d7320ad9f2b9db86cec4adb19fef9caa8b4ce9eb
Kai Chang <kaijchang@gmail.com>
2018-09-09 08:29:34 -0700

    cronitor integration

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
+
+curl "https://cronitor.link/$CRONITOR_ID/run" -m 10 || true
 python3 build.py
 git add .
 git commit -m 'Automated Weekly Rebuild'
 git push -u github master
+curl "https://cronitor.link/$CRONITOR_ID/complete" -m 10 || true
