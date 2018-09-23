564d2fcf9887e6479f6d59701d102d3e189cf95f
Kai Chang <kaijchang@gmail.com>
2018-09-23 16:22:57 -0700

    use python3 instead of specific version

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl "https://cronitor.link/$CRONITOR_ID/run" -m 10 || true
-/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 build.py
+/usr/local/bin/python3 build.py
 /usr/local/bin/git add .
 /usr/local/bin/git commit -m 'Automated Weekly Rebuild'
 curl "https://cronitor.link/$CRONITOR_ID/complete" -m 10 || true
